52075222b054545ddfd1196d5051243c6c2a503a
Phellype Guilherme <phellype.2020@gmail.com>
2024-04-11 01:03:16 -0300

    Adicionado Back end com integracao com front e H2

---
+++ Projeto/site-venha-junto/site-venha-junto/src/main/java/com/br/sitevenhajunto/SiteVenhaJuntoApplication.java
@@ -0,0 +1,16 @@
+package com.br.sitevenhajunto;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@Slf4j
+public class SiteVenhaJuntoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiteVenhaJuntoApplication.class, args);
+		log.info("http://localhost:8080/cadastro");
+	}
+
+}
